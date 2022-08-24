a63e9c93c4e37e9ee6713f836c05d25176b44f69
Anders Olson <sredna43@gmail.com>
2022-08-23 22:03:54 -0500

    readability

--- aoc15/src/main/kotlin/day6/Day6.kt
+++ aoc15/src/main/kotlin/day6/Day6.kt
@@ -40,26 +40,12 @@ class Day6 : Day<List<String>, Int>("Day 6") {
         }
     }
 
-    private fun turnOnTheLights2(start: Pair<Int, Int>, stop: Pair<Int, Int>) {
-        for (i in start.first..stop.first) {
-            for (j in start.second..stop.second) {
-                grid[i][j] = grid[i][j] + 1
-            }
-        }
-    }
+
 
     private fun turnOffTheLights(start: Pair<Int, Int>, stop: Pair<Int, Int>) {
         for (i in start.first..stop.first) {
             for (j in start.second..stop.second) {
                 grid[i][j] = 0
-            }
-        }
-    }
-
-    private fun turnOffTheLights2(start: Pair<Int, Int>, stop: Pair<Int, Int>) {
-        for (i in start.first..stop.first) {
-            for (j in start.second..stop.second) {
-                grid[i][j] = (grid[i][j] - 1).coerceAtLeast(0)
             }
         }
     }
@@ -76,30 +62,12 @@ class Day6 : Day<List<String>, Int>("Day 6") {
         }
     }
 
-    private fun toggleTheLights2(start: Pair<Int, Int>, stop: Pair<Int, Int>) {
-        for (i in start.first..stop.first) {
-            for (j in start.second..stop.second) {
-                grid[i][j] = grid[i][j] + 2
-            }
-        }
-    }
-
     private fun followInstruction(input: String) {
         val instruction = parseLine(input)
         when (instruction.type) {
             "on" -> turnOnTheLights(instruction.first, instruction.second)
             "off" -> turnOffTheLights(instruction.first, instruction.second)
             "toggle" -> toggleTheLights(instruction.first, instruction.second)
-            else -> println("unknown instruction: ${instruction.type}")
-        }
-    }
-
-    private fun followInstruction2(input: String) {
-        val instruction = parseLine(input)
-        when (instruction.type) {
-            "on" -> turnOnTheLights2(instruction.first, instruction.second)
-            "off" -> turnOffTheLights2(instruction.first, instruction.second)
-            "toggle" -> toggleTheLights2(instruction.first, instruction.second)
             else -> println("unknown instruction: ${instruction.type}")
         }
     }
@@ -116,6 +84,42 @@ class Day6 : Day<List<String>, Int>("Day 6") {
             }
         }
         return count
+    }
+
+    // Part Two Below, split for readability since they're very similar
+
+    private fun turnOnTheLights2(start: Pair<Int, Int>, stop: Pair<Int, Int>) {
+        for (i in start.first..stop.first) {
+            for (j in start.second..stop.second) {
+                grid[i][j] = grid[i][j] + 1
+            }
+        }
+    }
+
+    private fun turnOffTheLights2(start: Pair<Int, Int>, stop: Pair<Int, Int>) {
+        for (i in start.first..stop.first) {
+            for (j in start.second..stop.second) {
+                grid[i][j] = (grid[i][j] - 1).coerceAtLeast(0)
+            }
+        }
+    }
+
+    private fun toggleTheLights2(start: Pair<Int, Int>, stop: Pair<Int, Int>) {
+        for (i in start.first..stop.first) {
+            for (j in start.second..stop.second) {
+                grid[i][j] = grid[i][j] + 2
+            }
+        }
+    }
+
+    private fun followInstruction2(input: String) {
+        val instruction = parseLine(input)
+        when (instruction.type) {
+            "on" -> turnOnTheLights2(instruction.first, instruction.second)
+            "off" -> turnOffTheLights2(instruction.first, instruction.second)
+            "toggle" -> toggleTheLights2(instruction.first, instruction.second)
+            else -> println("unknown instruction: ${instruction.type}")
+        }
     }
 
     override fun partTwo(input: List<String>): Int {
